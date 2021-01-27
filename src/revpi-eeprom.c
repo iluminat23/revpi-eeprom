238d283c5bffbe842b7dcaae0d9650c5d83c7964
Philipp Rosenberger <p.rosenberger@kunbus.com>
2021-01-27 10:51:50 +0100

    Open image and check file size.
    Signed-off-by: Philipp Rosenberger <p.rosenberger@kunbus.com>

--- src/revpi-eeprom.c
+++ src/revpi-eeprom.c
@@ -17,7 +17,9 @@ int main(int argc, char *argv[])
 {
 	int ret;
 	int eeprom_fd;
+	int image_fd;
 	struct stat st;
+	size_t image_size;
 	/* FIXME: Use O_WRONLY, as we should not need to read the eeprom. */
 	eeprom_fd = open(EEPROM_PATH, O_RDWR | O_SYNC);
 	if (eeprom_fd < 0) {
@@ -35,6 +37,29 @@ int main(int argc, char *argv[])
 	if (st.st_size != EEPROM_SIZE) {
 		fprintf(stderr, "ERROR: EEPROM size missmatch, expacted: %d; got: %d\n",
 		        EEPROM_SIZE, st.st_size);
+		exit(EXIT_FAILURE);
+	}
+
+	if (argc < 1) {
+		fprintf(stderr, "ERROR: No image file specified.\n");
+		exit(EXIT_FAILURE);
+	}
+	image_fd = open(argv[1], O_RDONLY);
+	if (image_fd < 0) {
+		fprintf(stderr, "ERROR: Can't open image file `%s': %s\n", argv[1],
+		        strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	ret = fstat(image_fd, &st);
+	if (ret < 0) {
+		fprintf(stderr, "ERROR: Can't get image file status `%s': %s\n",
+		        argv[1], strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	image_size = st.st_size;
+	if (image_size > EEPROM_SIZE) {
+		fprintf(stderr, "ERROR: Image size (%d) exceeds EEPROM size (%d).\n",
+		        image_size, EEPROM_SIZE);
 		exit(EXIT_FAILURE);
 	}
 
